c5996ef62da09f11f854da85d2f886a6af332cfa
Edmond <ecukalla@gmail.com>
2019-12-16 17:18:24 +0100

    initial commit

---
+++ src/main/java/al/aws/workshop/awesomeapp/AwesomeappApplication.java
@@ -0,0 +1,13 @@
+package al.aws.workshop.awesomeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwesomeappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/al/aws/workshop/awesomeapp/AwesomeappApplicationTests.java
@@ -0,0 +1,13 @@
+package al.aws.workshop.awesomeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwesomeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
